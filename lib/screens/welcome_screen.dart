e0f8d455e8dec03855f3d4d11f4034b464205191
ShockSmoke <adityagupta0116@gmail.com>
2021-10-17 13:01:17 +0530

    added login

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+      print(controller.value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                DefaultTextStyle(
+                  style: TextStyle(color: Colors.black, fontSize: 40),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText('Flash Chat'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            Button(
+              text: "Login",
+              onpressed: (){
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              
+            ),
+            Button(
+              text: "Register",
+              onpressed: (){
+                print("called you");
+                Navigator.pushNamed(context,RegistrationScreen.id);
+              },
+              
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
